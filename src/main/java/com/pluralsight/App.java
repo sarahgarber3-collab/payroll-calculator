b61cc41962b361fa0f121e5278e9f2331413c968
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-08 15:51:30 -0400

    Exercise 4

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name here: ");
+        scanner.nextLine();
+        String name = "Sarah";
+
+        System.out.print("Enter hours worked: ");
+        float hours = scanner.nextFloat();
+        scanner.nextLine();
+
+        System.out.print("Enter pay rate: ");
+        float pay = scanner.nextFloat();
+        scanner.nextLine();
+
+
+        float grossPay = hours * pay;
+        System.out.println(grossPay);
+        System.out.println(name);
+
+
+
+    }
 }
